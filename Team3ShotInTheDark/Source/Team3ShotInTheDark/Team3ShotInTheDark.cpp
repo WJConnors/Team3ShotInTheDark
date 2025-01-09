0058ceda993ecf88ffe11a543a86d740fd22583b
WJConnors <billy.connors@yahoo.co.uk>
2025-01-09 11:13:14 +0000

    Remove all files from subdirectory to re-add with new .gitignore

--- Team3ShotInTheDark/Source/Team3ShotInTheDark/Team3ShotInTheDark.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Team3ShotInTheDark.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team3ShotInTheDark, "Team3ShotInTheDark" );
- 
